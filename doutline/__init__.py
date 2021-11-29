a84e874092a24b0d77c4882d3442a411544f4c4d
Cariad Eccleston <cariad@cariad.earth>
2021-11-29 12:57:01 +0000

    Add classes

--- doutline/__init__.py
+++ doutline/__init__.py
@@ -1,4 +1,11 @@
 from importlib.resources import open_text
 
+from doutline.outline import Outline, OutlineItem
+
 with open_text(__package__, "VERSION") as t:
     __version__ = t.readline().strip()
+
+__all__ = [
+    "Outline",
+    "OutlineItem",
+]
